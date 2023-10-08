77c6f57da7ce419dddad719d420a9e9400bff626
jhonatas <jhonatasgalvao00@gmail.com>
2023-10-08 12:40:26 -0300

    create tela login / redirect new page

---
+++ project/src/app/login/login.page.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule]
+})
+export class LoginPage implements OnInit {
+  username: string = '';
+  password: string = '';
+
+
+  constructor(private router: Router) { }
+
+  
+  ngOnInit() {
+  }
+  
+  login() {
+    // Aqui, você pode adicionar a lógica de validação do usuário e senha.
+    // Por exemplo, você pode verificar se o nome de usuário e a senha estão corretos.
+    // Se a validação for bem-sucedida, redirecione o usuário para a página principal.
+    if (this.username === 'usuario' && this.password === 'senha') {
+      // Login bem-sucedido, redirecione para a página principal
+      this.router.navigate(['/home']);
+    } else {
+      // Exiba uma mensagem de erro ou trate o login inválido de outra forma
+      console.log('Login inválido. Verifique suas credenciais.');
+    }
+  }
+}
