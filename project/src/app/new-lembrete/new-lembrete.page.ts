c5eaa3c01915aaacafa41ef33a97e1982cbb2999
jhonatas <jhonatasgalvao00@gmail.com>
2023-11-06 19:41:12 -0300

    config module alert , verify local-storage limit set

--- project/src/app/new-lembrete/new-lembrete.page.ts
+++ project/src/app/new-lembrete/new-lembrete.page.ts
@@ -29,7 +29,51 @@ export class NewLembretePage implements OnInit {
         telefone:['',Validators.required]
       });
   }
-
+  submitForm() {
+    if (this.meuForm.valid) {
+      const dadosFormulario = this.meuForm.value;
+      this.dadosService.setDados(dadosFormulario);
+  
+      if (!dadosFormulario.nome || !dadosFormulario.email) {
+        this.mostrarAlerta('Campos Vazios', 'Por favor, preencha todos os campos obrigatórios.');
+        return; // Encerre a função se os campos estiverem vazios
+      }
+  
+      this.storage.get('dadosFormulario').then((existingData) => {
+        let dataToSave: any[] = [];
+  
+        if (existingData && Array.isArray(existingData)) {
+          dataToSave = existingData;
+  
+          // Verifique se os novos dados já existem no array
+          const isDuplicate = dataToSave.some((item) => {
+            return JSON.stringify(item) === JSON.stringify(dadosFormulario);
+          });
+  
+          if (isDuplicate) {
+            console.log('Dados iguais, não serão salvos novamente.');
+            this.mostrarAlerta('Dados Iguais', 'Os dados já existem.');
+            return; // Encerre a função se os dados forem iguais
+          }
+        }
+  
+        if (this.selectedImage) {
+          dadosFormulario.imagem = this.selectedImage; // Adicione a imagem aos dados do formulário
+        }
+  
+        dataToSave.push(dadosFormulario); // Adicione os novos dados ao array
+  
+        this.storage.set('dadosFormulario', dataToSave).then(() => {
+          console.log('Dados do formulário (incluindo a imagem) salvos no Local Storage');
+        });
+      });
+  
+    } else {
+      this.mostrarAlerta('Campos Inválidos', 'Por favor, preencha os campos corretamente.');
+    }
+  }
+  
+  
   async ngOnInit() {
     await this.storage.create();
   }
@@ -49,54 +93,8 @@ export class NewLembretePage implements OnInit {
     await alert.present();
   }
 
-  submitForm() {
-    
-    if (this.meuForm.valid) {
-      const dadosFormulario = this.meuForm.value;
-      this.dadosService.setDados(dadosFormulario);
-
-    if (!dadosFormulario.nome || !dadosFormulario.email) {
-      this.mostrarAlerta('Campos Vazios', 'Por favor, preencha todos os campos obrigatórios.');
-      return; // Encerre a função se os campos estiverem vazios
-    }
   
-      this.storage.get('dadosFormulario').then((existingData) => {
-        if (existingData) {
-          // Se já existe algum dado salvo, você pode verificar se os novos dados são diferentes
-          if (JSON.stringify(existingData) !== JSON.stringify(dadosFormulario)) {
-            // Os dados são diferentes, então você pode salvar novamente
-
-            if (this.selectedImage) {
-              dadosFormulario.imagem = this.selectedImage; // Adicione a imagem aos dados do formulário
-            }
-
-            this.storage.set('dadosFormulario', dadosFormulario).then(() => {
-              console.log('Dados do formulário (incluindo a imagem) salvos no Local Storage');
-            });
-
-          } else {
-            console.log('Os dados são iguais, não há necessidade de salvar novamente.');
-            this.mostrarAlerta('2d2d2d','wefg');
-          }
-
-        } else {
-          // Nenhum dado existente encontrado, então você pode salvar diretamente
-          if (this.selectedImage) {
-            dadosFormulario.imagem = this.selectedImage; // Adicione a imagem aos dados do formulário
-          }
-          
-          this.storage.set('dadosFormulario', dadosFormulario).then(() => {
-            console.log('Dados do formulário (incluindo a imagem) salvos no Local Storage');
-          });
-        }
-        
-      });
-      
-    }else {
-      this.mostrarAlerta('Campos Inválidos', 'Por favor, preencha os campos corretamente.');
-    }
-    
-  }
+  
   onFileSelected(event: any) {
     const file = event.target.files[0];
     const reader = new FileReader();
